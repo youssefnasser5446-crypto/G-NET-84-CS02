b2bfcad56bb905af929161bdb25c739f98e01118
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-07-19 19:31:44 +0300

    Question02

--- Assignment_Session3_C#/Program.cs
+++ Assignment_Session3_C#/Program.cs
@@ -23,6 +23,30 @@
             }
 
             #endregion
+
+            #region Question02
+            /*
+             2. Given string title = "Refactoring";, 
+            write a switch statement that prints "Great choice!"
+            if the title is "Clean Code", 
+            "Nice pick!" if it's "Refactoring",
+            and "Never heard of it" for anything else. 
+             */
+            string title = "Refactoring";
+
+            switch (title)
+            {
+                case "Clean Code":
+                    Console.WriteLine("Great choice!");
+                    break;
+                case "Refactoring":
+                    Console.WriteLine("Nice pick!");
+                    break;
+                default:
+                    Console.WriteLine("Never heard of it");
+                    break;
+            }
+            #endregion
         }
     }
 }
