c7da754e66291f9cdf3cafd72a252502d0e21e4d
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-07-19 19:20:39 +0300

    Question01

--- Assignment_Session3_C#/Program.cs
+++ Assignment_Session3_C#/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+
+            /*
+             
+            1. Given int pages = 464; and bool isAvailable = true;,
+            print "You can borrow this book" only
+            if pages is greater than 300 and isAvailable is true.
+            Use the && operator.
+              
+            */
+
+            int pages = 464;
+            bool isAvailable = true;
+            if (pages > 300 && isAvailable)
+            {
+                Console.WriteLine("You can borrow this book");
+            }
+
+            #endregion
         }
     }
 }
